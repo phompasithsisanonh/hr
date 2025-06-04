d21f6c23d58a7978527d0ea11552540bb08749ab
phompasith sisanonh <98532227+phompasithsisanonh@users.noreply.github.com>
2025-06-04 21:31:34 +0700

    h

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : 'https://my-worker.kingsatur16.workers.dev/api'
+    baseURL : 'https://hrbackend-6pfl.onrender.com/api'
 })
 
 export default api
